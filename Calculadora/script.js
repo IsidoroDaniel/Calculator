eddc15f22c1f2daa5dbc07780a34e46356c3c4af
IsidoroDaniel <106537229+IsidoroDaniel@users.noreply.github.com>
2022-08-12 15:33:07 -0300

    Add files via upload

---
+++ Calculadora/script.js
@@ -0,0 +1,8 @@
+const calculator = document.querySelector(‘.calculadora-corpo`)
+const keys = calculator.querySelector(‘.calculadora-corpo-botoes’)
+
+keys.addEventListener(‘click’, e => {
+ if (e.target.matches(‘button’)) {
+   // Fazer algo
+ }
+})
