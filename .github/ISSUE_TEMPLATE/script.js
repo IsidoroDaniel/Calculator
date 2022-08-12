3f41b5c03c42a604f8344dbaa603e059454a3047
IsidoroDaniel <106537229+IsidoroDaniel@users.noreply.github.com>
2022-08-12 15:32:15 -0300

    Delete .github/ISSUE_TEMPLATE directory

--- .github/ISSUE_TEMPLATE/script.js
+++
@@ -1,8 +0,0 @@
-const calculator = document.querySelector(‘.calculadora-corpo`)
-const keys = calculator.querySelector(‘.calculadora-corpo-botoes’)
-
-keys.addEventListener(‘click’, e => {
- if (e.target.matches(‘button’)) {
-   // Fazer algo
- }
-})
